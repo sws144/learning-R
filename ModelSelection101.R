4a04c1e23d8c2304c04f8e2cffc164453721544c
Stanley Wang <s.wang4331@gmail.com>
2019-06-11 23:38:22 -0400

    model sel

--- ModelSelection101.R
+++ ModelSelection101.R
@@ -4,3 +4,16 @@
 
 rm(list=ls())
 
+#load data and convert to 
+dat <- read.table("employment_data.txt", header = T)
+names(dat) <- c("employed", "foreigner", "gov.support", "age", "education", "young.children", "school.children")
+levels(dat$employed)[1] <- 'yes'
+levels(dat$employed)[2] <- 'no'
+levels(dat$foreigner)[1] <- 'yes'
+levels(dat$foreigner)[2] <- 'no'
+
+str(dat)
+summary(dat)
+
+fit.1 <- glm(employed =="yes" ~ ., data = dat , family = binomial)
+summary(fit.1)
