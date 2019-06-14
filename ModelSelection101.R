4cf6614a75cf9aeb60d01a0927d757187b7770ad
Stanley Wang <s.wang4331@gmail.com>
2019-06-13 20:06:39 -0400

    6/13/2019

--- ModelSelection101.R
+++ ModelSelection101.R
@@ -17,3 +17,33 @@ summary(dat)
 
 fit.1 <- glm(employed =="yes" ~ ., data = dat , family = binomial)
 summary(fit.1)
+
+tempfit <- update(fit.1, .~. + factor(young.children == 0)
+                              + factor(school.children == 0)
+                              + factor(young.children + school.children ==0))
+summary(tempfit)
+
+
+logit.plot <- function(x, y, h, link='logit', xlab=deparse(substitute(x)), yname=deparse(substitute(y)), 
+                       ylab, rug=T, data, main){
+  if(!missing(data)){
+    call <- match.call()
+    dataPos <- match("data",names(call))
+    return(invisible(with(data, eval(call[-dataPos]))))
+  }
+  if (length(levels(factor(y)))!=2) stop('y must be binary')
+  succes <- levels(factor(y))[2]
+  if (missing(ylab)) ylab <- paste(binomial(link)$link,' P{',yname,'=',succes,'|',xlab,'}', sep='', collapse='')
+  if (is.factor(y)) y <- as.numeric(y==levels(y)[2])
+  x.seq <- seq(min(x),max(x),length=101)
+  smooth <- binomial(link)$linkfun(ksmooth(x, y, 'normal', h, x.points=x.seq)$y)
+  plot(smooth~x.seq, ylab=ylab, xlab=xlab, type='l', main = main)
+  if (rug) rug(x)
+  invisible(xy.coords(x = x.seq, y = smooth, xlab = xlab, ylab = ylab))
+}
+
+num = 1
+for(i in seq(2.5,10,length.out = 6)){
+  logit.plot(x = age, y = employed == 'yes', h = i, data = dat, main = paste0("plot: ", num, ", bandwidth = ",i))
+  num= num +1
+}
